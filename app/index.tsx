32bf7ba73ebed7cedf64ca2f4401f18c2e814ffe
Alejandro Apodaca <apoapps@Alejandros-Mac-mini.local>
2025-10-15 12:35:39 -0700

    clase 45

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,27 @@
-
-
-import { globalStyles } from '@/styles/global-styles';
-import React from 'react';
-import { Text, View } from 'react-native';
+import CalculatorButton from "@/components/CalculatorButton";
+import ThemeText from "@/components/ThemeText";
+import { Colors } from "@/constants/Colors";
+import { globalStyles } from "@/styles/global-styles";
+import React from "react";
+import { View } from "react-native";
 
 const CalculatorApp = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
-      <Text style={globalStyles.mainResult}>50 x 50</Text>
-      <Text style={globalStyles.subResult}>250</Text>
-
+      {/* Filas de botones */}
+      <View style={{ paddingHorizontal: 30, marginBottom: 20 }}>
+        <ThemeText variant="h1">50 x 50</ThemeText>
+        <ThemeText variant="h2">250</ThemeText>
+      </View>
+      {/* Filas de botones */}
+      <View style={globalStyles.row}>
+        <CalculatorButton label="C" onPress={() => console.log("C")} blackText color={Colors.darkGray} />
+        <CalculatorButton label="+/-" onPress={() => console.log("+/-")} blackText color={Colors.darkGray} />
+        <CalculatorButton label="del" onPress={() => console.log("del")} blackText color={Colors.darkGray} />
+        <CalculatorButton label="÷" onPress={() => console.log("÷")} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default CalculatorApp
+export default CalculatorApp;
